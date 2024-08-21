1aa7545cb93ae5a3e0595df9a099de2b38fc7c0a
EC2 Default User <ec2-user@ip-172-31-46-234.eu-north-1.compute.internal>
2024-08-21 16:57:53 +0000

    directory and file is created

---
+++ files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p India/{Telangana/{Hyderabad,Suryapet,Nalgonda},Andhra\ Pradesh/{Vijayawada,West\ Godavari,East\ Godavari},Karnataka/{Bangalore,Mysore,Hubli}}
+
+touch India/Telangana/Hyderabad/{a1.txt,a2.txt,a3.txt}
+touch India/Telangana/Suryapet/{a1.txt,a2.txt,a3.txt}
+touch India/Telangana/Nalgonda/{a1.txt,a2.txt,a3.txt}
+
+touch India/Andhra\ Pradesh/Vijayawada/{b1.txt,b2.txt,b3.txt}
+touch India/Andhra\ Pradesh/West\ Godavari/{b1.txt,b2.txt,b3.txt}
+touch India/Andhra\ Pradesh/East\ Godavari/{b1.txt,b2.txt,b3.txt}
+
+touch India/Karnataka/Bangalore/{b1.txt,b2.txt,b3.txt}
+touch India/Karnataka/Mysore/{b1.txt,b2.txt,b3.txt}
+touch India/Karnataka/Hubli/{b1.txt,b2.txt,b3.txt}
+
+echo "Directory structure created successfully!"
